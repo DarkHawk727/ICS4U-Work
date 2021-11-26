7cd508f3b5191bd04b494b1b7370b224b3e8ef0d
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-11-25 19:12:23 -0500

    updates

--- Assignments/File Assignment 3/Part1.py
+++ Assignments/File Assignment 3/Part1.py
@@ -1,22 +1,32 @@
-text = open("Assignments/File Assignment 3/Accompanying Files/sample_text.txt", 'r')
+def quicksort(array):
+	if len(array) <= 1:
+		return array
+
+	pivot = array[0]
+	less = [elem for elem in array[1:] if elem <= pivot]
+	greater = [elem for elem in array[1:] if elem > pivot]
+
+	return quicksort(less) + [pivot] + quicksort(greater)
+
+
 words_list = []
 word_frequency = {}
+with open("Assignments/File Assignment 3/Accompanying Files/sample_text.txt", 'r') as text:
+	for line in text.readlines():
+		line = line.strip().lower()
 
-for line in text.readlines():
-	line = line.strip().lower()
+		for punc in "!?\":;,()\'.*[]{}":
+			line = line.replace(punc, "")
 
-	for punc in "!?\":;,()\'.*[]{}":
-		line = line.replace(punc, "")
+		words_list = line.split(" ")
 
-	words_list = line.split(" ")
-
-	for word in words_list:
-		if not word:
-			continue
-		try:
-			word_frequency[word] += 1
-		except KeyError:
-			word_frequency[word] = 1
+		for word in words_list:
+			if not word:
+				continue
+			try:
+				word_frequency[word] += 1
+			except KeyError:
+				word_frequency[word] = 1
 
 # Create your own sorting algo
 sorted_frequency = sorted(word_frequency.items(), key=lambda item: item[1], reverse=True)
