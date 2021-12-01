d010e20414f019f3fd636970caf99e86be9e5096
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-12-01 09:55:08 -0500

    updates

--- Assignments/Review Assignment 2/Assign23.py
+++ Assignments/Review Assignment 2/Assign23.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 board = np.zeros((8, 8), dtype=int)
-
+won = False
+player = 1
 
 def print_board(board):
 	for row in board:
@@ -30,23 +31,40 @@ def place_piece(board, player, choice):
 # TODO: Add a function that checks if the board is full
 # TODO: Finish the check_win function
 def check_win(board, player):
+	temp_bool = False
 	win_count = 0
 	temp_board = board.tolist()
-	for row in range(7):
-		for column in range(7):
-			if temp_board[row:column] != 0:
-				if temp_board[row:column] == temp_board[row+1:column+1] == temp_board[row+2:column+2] == temp_board[row+1:column+2] == temp_board[row:column+3]:
-					won = True 
-	return won
+	regular_v, upside_down_v = [], []
+	print(temp_board)
+	for row in range(6):
+		for column in range(4):
+			if temp_board[row][column] > 0:
+				regular_v.append(temp_board[row][column])
+				regular_v.append(temp_board[row][column+4])
+				regular_v.append(temp_board[row+1][column+1])
+				regular_v.append(temp_board[row+1][column+2])
+				regular_v.append(temp_board[row+2][column+2])
+				print(regular_v)
+			
+				if all(regular_v) == 1:
+					temp_bool = True
+				elif all(regular_v) == 2:
+					temp_bool = True
+			regular_v = []
+				# if temp_board[row][column] == temp_board[row][column+4] == temp_board[row+1][column+1] == temp_board[row+1][column+2] == temp_board[row+2][column+2]:
+	return temp_bool
 
 
-won = False
-player = 1
 
 while not won:
 	print_board(board)
 	raw_input = input("Please select a column (A-H): ")
 	choice = ord(raw_input.lower()) - 97
 	place_piece(board, player, choice)
-	check_win(board, player)
+	if check_win(board, player):
+		print("Player " + str(player) + " wins!")
+		won = True
+	if np.all(board):
+		print("Tie!")
+		won = True
 	player = 3 - player
