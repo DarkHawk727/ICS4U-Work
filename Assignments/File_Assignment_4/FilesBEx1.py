7e15ca36b1734f4a04007403ee07b90d0941658e
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-12-06 18:59:04 +0000

    more work done

--- Assignments/File_Assignment_4/FilesBEx1.py
+++ Assignments/File_Assignment_4/FilesBEx1.py
@@ -11,27 +11,34 @@ def load_file(filepath):
         table = np.loadtxt(data, dtype=datatype, skiprows=1)
     return table
 
+
 def save_file(table, filepath):
-	np.savetxt(filepath, table)
+	# TODO: Add format specifier for saving the textt.
+	np.savetxt(filepath, table, fmt=datatype)
 
 
-def insert_employee(employee_number, name, maritial_status, age, salary,
-                    table):
+def add_employee(employee_number, name, maritial_status, age, salary, table):
+	# TODO: Check to see if there are two duplicate employee ids
     temp_row = np.empty((1, ), dtype=datatype)
     temp_row["Employee#"] = employee_number
     temp_row["Name"] = name
     temp_row["Marital Status"] = maritial_status
     temp_row["Age"] = age
     temp_row["Salary"] = salary
-    np.append(table, temp_row)
+    return np.append(table, temp_row)
 
 
 def delete_employee(table, index):
-    np.delete(table, index, 0)
+	# TODO: Add out of bounds error catching
+    return np.delete(table, index, 0)
 
 
-def modify_table(table):
-	# TODO: Maybe, idk.... write the damn function. Just a thought....
+def modify_table(employee_number, name, maritial_status, age, salary, table):
+	# TODO: Check to see if there are two duplicate employee ids
+
+	display_all(table)
+	index = int(input("Please select the index of the employee you would like to modify: "))
+	table[]
 	pass
 
 
@@ -43,32 +50,34 @@ def display_all(table):
                             numalign="center")
     print(pretty_table)
 
+print(
+"""1. Open File
+2. Save File
+3. Add a record
+4. Delete a record
+5. Modify a record
+6. Display All
+7. Quit""")
 choice = 0	
 while choice != 7:
 	choice = int(input("Please enter a choice (1-7): "))
 	if choice == 1:
-		t = load_file("employees.txt")
-		choice = int(input("Please enter a choice (1-7): "))
+		t = load_file("Assignments/File_Assignment_4/employees.txt")
 	elif choice == 2:
 		save_file(t, "employees.txt")
-		choice = int(input("Please enter a choice (1-7): "))
 	elif choice == 3:
 		employee_number = str(input("Please enter the employee number: "))
 		name = str(input("Please enter the name ([First Initial].[Last Name]: "))
 		maritial_status = str(input("Please enter the employee's maritial status (M or S): ")).upper()
 		age = int(input("Please enter the age of the employee: "))
 		salary = int(input("Please enter the employee's salary: ")) 
-		insert_employee(index, employee_number, name, maritial_status, age, salary,t)
-		choice = int(input("Please enter a choice (1-7): "))
+		t = add_employee(employee_number, name, maritial_status, age, salary,t)
 	elif choice == 4:
 		index = int(input("Please enter the index of the employee you wish to delete: "))
-		delete_employee(t, index)
-		choice = int(input("Please enter a choice (1-7): "))
+		t = delete_employee(t, index)
 	elif choice == 5:
 		# TODO: add modify record
-		choice = int(input("Please enter a choice (1-7): "))
 		pass
 	elif choice == 6:
 		display_all(t)
-		choice = int(input("Please enter a choice (1-7): "))
-display_all(t)
+
--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Repository for my work in Grade 12 Computer Science (ICS4U)
 
 # TODO
  - [ ] Finish `Part2b.py` in *File Assignment 3*
- - [ ] Finish `FilesBEx1.py` in *File Assignment 4*
+ - [ ] Work on `modify_record` in  `FilesBEx1.py` in *File Assignment 4*
  - [ ] Finish `Part4.py` and `Part5.py` in *Recursion Assignment 5*
