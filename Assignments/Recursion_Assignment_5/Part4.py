7624a31b0b9a733fcd080423c489ad848d8e1b42
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-12-08 17:21:32 -0500

    finished Part4.py

--- Assignments/Recursion_Assignment_5/Part4.py
+++ Assignments/Recursion_Assignment_5/Part4.py
@@ -1,19 +1,12 @@
 def calculate_patterns(previous_term, index, counter):
-	if counter == previous_term:
-		return previous_term
-	
-	if index % 2 != 0:
-		counter += 1
-		previous_term *= 2
-		previous_term += 3
-		return calculate_patterns(previous_term, index, counter)
-	else:
-		counter += 1
-		previous_term -= 2
-		return calculate_patterns(previous_term, index, counter)
-	return calculate_patterns(previous_term, index, counter)
+    if counter == index:
+        return previous_term
+    if counter % 2 == 0:
+        return calculate_patterns(2 * previous_term + 3, index, counter + 1)
+    else:
+        return calculate_patterns(previous_term - 2, index, counter + 1)
 
-		
+
 initial_term = int(input("Please enter the initial term: "))
-index = int(input("Please enter the index: "))
-print(calculate_patterns(initial_term, index, index), end=', ')
+index = int(input("Please enter the index: ")) - 1
+print(calculate_patterns(initial_term, index, 0))
