9615d4c49732581bd728abbc84c3e60bd7e60483
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-12-15 17:28:29 -0500

    changed file names lol

--- Assignments/Recursion_Assignment_5/Part5.py
+++ Assignments/Recursion_Assignment_5/Part5.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def recursivley_find_longest_palindrome(input, i, inc, pals, size):
     if i > len(input):
         return pals
@@ -20,17 +19,3 @@ def is_Palindrome(input):
 
 pals = ""
 print(recursivley_find_longest_palindrome("Some like cake but I prefer pie", 0, 1, pals, 0))
-=======
-def recursivley_find_longest_palindrome(string, palindrome, start_index, end_index):
-	 
-
-
-def check_palindrome(string):
-	if string.lower().replace(" ", "") == string[::-1].lower().replace(" ", ""):
-		return True
-	else:
-		return False 
-
-
-print(recursivley_find_longest_palindrome("Some like cake but I prefer pie"))
->>>>>>> ae857b8b29b93c6a2781996f2c67d0fc37d15a6a
--- Assignments/Recursion_Assignment_6/Part1/MazeSolver.py
+++ Assignments/Recursion_Assignment_6/Part1/MazeSolver.py
@@ -1,9 +1,17 @@
 import numpy as np
 
-with open("Assignments/Recursion_Assignment_6/Part1/Sample_Files/File1.txt", 'r') as file:
+file_number = str(input("Enter the file number: "))
+filename = "Assignments/Recursion_Assignment_6/Part1/Sample_Files/Maze" + file_number + ".txt"
+
+with open(filename, 'r') as file:
 	raw_input = file.readline().split(",")
 	x_dim, y_dim = int(raw_input[0]), int(raw_input[1])
-	maze = np.array([list(char) for char in file.readlines()])
+# write code to read the file and store the maze in a 2D numpy array
+	maze = np.empty((x_dim, y_dim), dtype=str)
+	for i in range(x_dim):
+		for j in range(y_dim):
+			maze[i,j] = str(file.read(1))
+
 
 print(x_dim, y_dim)
 print(maze)
