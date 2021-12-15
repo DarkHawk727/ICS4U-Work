69b7be3a223fc1f81d9ad4c8d25dc11e213b7934
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-12-15 17:16:14 -0500

    bruh

--- Assignments/Recursion_Assignment_5/Part5.py
+++ Assignments/Recursion_Assignment_5/Part5.py
@@ -1,8 +1,21 @@
-# Write a program to find the longest palindromic substring of a given string.
-# You cannot use for loops or while loops.
-def recursivley_find_longest_palindrome(string):
+def recursivley_find_longest_palindrome(input, i, inc, pals, size):
+    if i > len(input):
+        return pals
+    j = i+inc
+    if j <= len(input):
+        if is_Palindrome(input[i:j]):
+            if len(input[i:j]) > size:
+                pals = input[i:j]
+        return recursivley_find_longest_palindrome(input, i, inc+1, pals, len(pals))
+    else:
+        return recursivley_find_longest_palindrome(input, i+1, 1, pals, len(pals))
 
 
+def is_Palindrome(input):
+    tempWord = input.replace(' ', '').lower()
+    word = tempWord[::-1]
+    return word == tempWord
 
 
-print(recursivley_find_longest_palindrome("some like cake but i prefer pie"))
+pals = ""
+print(recursivley_find_longest_palindrome("Some like cake but I prefer pie", 0, 1, pals, 0))
