6673eae8b79da99faffe6be0a2df4de69ce0f415
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-12-16 09:50:53 -0500

    working on the mazesolver

--- Assignments/Recursion_Assignment_6/Part1/MazeSolver.py
+++ Assignments/Recursion_Assignment_6/Part1/MazeSolver.py
@@ -1,17 +1,45 @@
 import numpy as np
 
+
+def find_path(maze, x, y):
+    if x < 0 or x >= x_dim or y < 0 or y >= y_dim:
+        return False
+    elif maze[x, y] == "x":
+        return True
+    elif maze[x, y] == ".":
+        maze[x, y] = "+"
+        return (
+            find_path(maze, x, y - 1)  # North
+            or find_path(maze, x, y + 1)  # South
+            or find_path(maze, x - 1, y)  # West
+            or find_path(maze, x + 1, y)  # East
+        )
+    elif maze[x, y] == "+":
+        maze[x, y] = "."
+        return (
+            find_path(maze, x, y - 1)  # North
+            or find_path(maze, x, y + 1)  # South
+            or find_path(maze, x - 1, y)  # West
+            or find_path(maze, x + 1, y)  # East
+        )
+    return False
+
+
 file_number = str(input("Enter the file number: "))
-filename = "Assignments/Recursion_Assignment_6/Part1/Sample_Files/Maze" + file_number + ".txt"
+filename = (
+    "Assignments/Recursion_Assignment_6/Part1/Sample_Files/Maze" + file_number + ".txt"
+)
 
-with open(filename, 'r') as file:
-	raw_input = file.readline().split(",")
-	x_dim, y_dim = int(raw_input[0]), int(raw_input[1])
-# write code to read the file and store the maze in a 2D numpy array
-	maze = np.empty((x_dim, y_dim), dtype=str)
-	for i in range(x_dim):
-		for j in range(y_dim):
-			maze[i,j] = str(file.read(1))
+with open(filename, "r") as file:
+    raw_input = file.readline().split(",")
+    x_dim, y_dim = int(raw_input[0]), int(raw_input[1])
 
+    maze = np.zeros((x_dim, y_dim), dtype=str)
+    for i in range(x_dim):
+        raw_input = file.readlines()
+        for j in range(y_dim):
+            maze[i, j] = raw_input[j]
 
-print(x_dim, y_dim)
+initial_x, initial_y = np.where(maze == "e")  # Find the starting point
+print((find_path(maze, initial_x, initial_y)))
 print(maze)
