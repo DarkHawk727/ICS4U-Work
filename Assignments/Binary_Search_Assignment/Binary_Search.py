2f9207c25d00e8e06ba0e5a786d99579e72e9d24
Arjun Sarao <47152801+DarkHawk727@users.noreply.github.com>
2021-12-25 17:04:05 -0500

    added blank files for future assignments

--- Assignments/Binary_Search_Assignment/Binary_Search.py
+++ Assignments/Binary_Search_Assignment/Binary_Search.py
@@ -1,10 +1,12 @@
-words = []
-with open("Assignments/Binary_Search_Assignment/dictionary.txt", 'r') as dictionary:
-	words.append(dictionary.read().strip())
+import numpy as np
 
-words.sort()
+dictionary_words = []
+with open("Assignments/Binary_Search_Assignment/dictionary.txt", 'r') as dictionary:
+	dictionary_words.append(dictionary.read().strip())
+
 
 def binary_search(arr, high, low, element):
+	arr.sort()
 	if high >= low:
 		middle = (high+low) // 2
 
@@ -13,14 +15,46 @@ def binary_search(arr, high, low, element):
 
 		elif arr[mid] > element:
 			return binary_search(arr, mid-1, low, element)
-		
 		else:
-			return binary_search
-				return binary_search(arr, mid+1, low, element)
+			return binary_search(arr, mid+1, low, element)
 	
 	else:
 		return -1
 
-word = str(input("Please enter a string: "))
 
-if binary_search(words, )
+def find_words(arr, words):
+    for elem in arr:
+		try:
+			if elem.find(word) > -1 or elem[::-1].find(word) > -1:
+				print("{} was found in the board".format(word))
+				words.remove(word)
+		except ValueError:
+			pass
+
+# 1. User enters a word
+# 2. Program checks if the word is in the dictionary using binary search
+# 3. Program checks if the word is in the board
+
+word = str(input("Please enter a word: "))
+
+rows, columns, diagonals = [], [], []
+
+with open("Assignments/File Assignment 3/Accompanying Files/grid.txt", "r") as f:
+    grid = f.read().splitlines()
+n = int(grid[0])
+grid = grid[1:]
+board = np.array([i for elem in grid for i in elem]).reshape(n, n)
+
+[rows.append("".join(board[:, i])) for i in range(n)]
+
+[columns.append("".join(board[i, :])) for i in range(n)]
+
+# Create the diagonals using np.diagonal() and some clever indexing
+diags = [board[::-1, :].diagonal(i) for i in range(-n + 1, n)]
+diags.extend(board.diagonal(i) for i in range(n - 1, -n, -1))
+
+[diagonals.append("".join(elem)) for elem in diags]
+
+find_words(rows, word)
+find_words(columns, word)
+find_words(diagonals, word)
--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Repository for my work in Grade 12 Computer Science (ICS4U)
 
 - [ ] Finish `Part5.py` in _Recursion Assignment 5_
 - [ ] Finish `MazeSolver.py` in _Recursion Assignment 6_
+- [ ] Finish `CCC_13_S4.py` in _Graph Assignments_
+- [ ] Finish `ECOO_14_R3_P2.py` in _Graph Assignments_
+- [ ] Finish `Binary_Search.py` in _Binary Search Assignment_
